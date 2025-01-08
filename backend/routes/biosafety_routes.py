96b823b4396fd2a4a4acd2a80064ae2ee6146e97
Robonor <robonor@apexsecurityint.com>
2025-01-08 21:15:52 +0000

    up

--- backend/routes/biosafety_routes.py
+++
@@ -1,9 +0,0 @@
-
-# biosafety_routes.py
-from flask import Blueprint
-
-biosafety_bp = Blueprint('biosafety', __name__)
-
-@biosafety_bp.route('/')
-def index():
-    return "This is the biosafety route."
--- backend/routes/outbreaks_routes.py
+++
@@ -1,9 +0,0 @@
-
-# outbreaks_routes.py
-from flask import Blueprint
-
-outbreaks_bp = Blueprint('outbreaks', __name__)
-
-@outbreaks_bp.route('/')
-def index():
-    return "This is the outbreaks route."
