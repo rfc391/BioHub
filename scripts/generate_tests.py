d3762f7dd05519f17b23fc9bbb2e7d8b23bb27e5
Robonor <robs@apexsecurityint.com>
2025-01-08 12:55:12 +0000

    Create generate_tests.py
    Signed-off-by: Robonor <robs@apexsecurityint.com>

---
+++ scripts/generate_tests.py
@@ -0,0 +1,18 @@
+import openai
+import os
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+def generate_test_cases(code_snippet):
+    prompt = f"Write test cases for the following Python code:\n{code_snippet}"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150
+    )
+    return response['choices'][0]['text']
+
+# Example usage
+with open("your_code.py", "r") as file:
+    code = file.read()
+print(generate_test_cases(code))
