3d7093a65cde3b505267fe6a5fd9d6cdb6f75ad5
Robonor <robs@apexsecurityint.com>
2025-01-08 21:53:16 +0000

    Update process_and_explain.py
    Signed-off-by: Robonor <robs@apexsecurityint.com>

--- scripts/process_and_explain.py
+++ scripts/process_and_explain.py
@@ -1,5 +1,5 @@
 import cv2
-from xaitk_saliency.impls.image_scoring import SaliencyScorer
+from xaitk_saliency.impls.perturb_image.mc_rise.MCRISEGrid import SaliencyScorer
 
 def process_image_with_opencv(image_path):
     # Load an image using OpenCV
